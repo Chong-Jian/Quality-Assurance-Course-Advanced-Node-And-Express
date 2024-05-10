029a2d7185437553d67cb4045b4ecd7d95e37f17
Chong Jian Ho <hochongjian@gmail.com>
2024-05-10 08:22:32 +0000

    gitpod /workspace/boilerplate-advancednode (main) $ git remote set-url origin https://github.com/Chong-Jian/Quality-Assurance-Course-Advanced-Node-And-Express.git gitpod /workspace/boilerplate-advancednode (main) $ git branch -M main gitpod /workspace/boilerplate-advancednode (main) $ git push -u origin main

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const res = require('express/lib/response.js');
 
 const app = express();
 
@@ -10,7 +11,10 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 
+res.render('index');
 app.route('/').get((req, res) => {
 
 });
